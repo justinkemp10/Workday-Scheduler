a724723cae9714df525212c6e04ef7978f565d66
Justin Kemp <jkemp0790@gmail.com>
2021-09-21 23:47:51 -0500

    condensed js file and finished writing app functionality

--- script.js
+++ script.js
@@ -1,135 +1,59 @@
-// use jQuery to provide interactive calendar
-
 // assign const for date & tie to element in the DOM
 const d = new Date();
-var now = d.getUTCHours();
+let now = moment(d).format("HH");
 currentTime = moment(d).format('MMMM d, YYYY' + ' h:mm A');
 
-$('#currentDay').text(currentTime);
-console.log(now);
-// TODO: assign variables and tie to elements in the DOM
-function eightAM(now) {
-    if (now < 8) {
-        $(".eightAM-input").addClass("future")
-    } else if (now > 8 || now < 9) {
-        $(".eightAM-input").addClass("present")
-    } else {
-        $(".eightAM-input").addClass("past")
+document.querySelector('#currentDay').textContent = currentTime;
+// assign variables for save button and user input, add event listener on click of save button to save user input in the DO
+
+// assign variable array for each hour of the planner
+let events = {
+    8: '',
+    9: '',
+    10: '',
+    11: '',
+    12: '',
+    13: '',
+    14: '',
+    15: '',
+    16: '',
+    17: '',
+}
+
+function addClickHandlers() {
+    let saveButton;
+    for (let i = 8; i < 18; i++) {
+        saveButton = document.querySelector('#save' + i);
+        saveButton.addEventListener('click', (e) => {
+            let hour = e.target.dataset.hour;
+            let event = document.querySelector('#hour' + hour);
+            events[hour] = event.value;
+            localStorage.setItem('events', JSON.stringify(events));
+        });
     }
 }
 
-function nineAM(now) {
-    if (now < 9) {
-        $(".nineAM-input").addClass("future")
-    } else if (now > 9 || now < 10) {
-        $(".nineAM-input").addClass("present")
-    } else {
-        $(".nineAM-input").addClass("past")
+function loadEvents() {
+    let timeSlot;
+
+    if (localStorage.getItem('events')) {
+        events = JSON.parse(localStorage.getItem('events'));
+    }
+
+    for (let i = 8; i < 18; i++) {
+        timeSlot = document.querySelector('#hour' + i);
+        timeSlot.value = events[i];
+        if (i < now) {
+            timeSlot.classList.add('past');
+        } else if (i > now) {
+            timeSlot.classList.add('future');
+        } else {
+            timeSlot.classList.add('present');
+        }
     }
 }
 
-function tenAM(now) {
-    if (now < 10) {
-        $(".tenAM-input").addClass("future")
-    } else if (now > 10 || now < 11) {
-        $(".tenAM-input").addClass("present")
-    } else {
-        $(".tenAM-input").addClass("past")
-    }
-}
-function elevenAM(now) {
-    if (now < 11) {
-        $(".elevenAM-input").addClass("future")
-    } else if (now > 11 || now < 12) {
-        $(".elevenAM-input").addClass("present")
-    } else {
-        $(".elevenAM-input").addClass("past")
-    }
-}
-function twelveAM(now) {
-    if (now < 12) {
-        $(".twelveAM-input").addClass("future")
-    } else if (now > 12 || now < 13) {
-        $(".twelveAM-input").addClass("present")
-    } else {
-        $(".twelveAM-input").addClass("past")
-    }
-}
-function onePM(now) {
-    if (now < 13) {
-        $(".onePM-input").addClass("future")
-    } else if (now > 13 || now < 14) {
-        $(".onePM-input").addClass("present")
-    } else {
-        $(".onePM-input").addClass("past")
-    }
-}
-function twoPM(now) {
-    if (now < 14) {
-        $(".twoPM-input").addClass("future")
-    } else if (now > 14 || now < 15) {
-        $(".twoPM-input").addClass("present")
-    } else {
-        $(".twoPM-input").addClass("past")
-    }
-}
-function threePM(now) {
-    if (now < 15) {
-        $(".threePM-input").addClass("future")
-    } else if (now > 15 || now < 16) {
-        $(".threePM-input").addClass("present")
-    } else {
-        $(".threePM-input").addClass("past")
-    }
-}
-function fourPM(now) {
-    if (now < 16) {
-        $(".fourPM-input").addClass("future")
-    } else if (now > 16 || now < 17) {
-        $(".fourPM-input").addClass("present")
-    } else {
-        $(".fourPM-input").addClass("past")
-    }
-}
-function fivePM(now) {
-    if (now < 17) {
-        $(".fivePM-input").addClass("future")
-    } else if (now > 17 || now < 18) {
-        $(".fivePM-input").addClass("present")
-    } else {
-        $(".fivePM-input").addClass("past")
-    }
-}
-
-function sixPM(now) {
-    if (now < 18) {
-        $(".sixPM-input").addClass("future")
-    } else if (now > 18 || now < 19) {
-        $(".sixPM-input").addClass("present")
-    } else {
-        $(".sixPM-input").addClass("past")
-    }
-}
-
-function sevenPM(now) {
-    if (now < 19) {
-        $(".sevenPM-input").addClass("future")
-    } else if (now > 19 || now < 20) {
-        $(".sevenPM-input").addClass("present")
-    } else {
-        $(".sevenPM-input").addClass("past")
-    }
-}
-
-eightAM();
-nineAM();
-tenAM();
-elevenAM();
-twelveAM();
-onePM();
-twoPM();
-threePM();
-fourPM();
-fivePM();
-sixPM();
-sevenPM();
+$(document).ready(() => {
+    addClickHandlers();
+    loadEvents();
+})
